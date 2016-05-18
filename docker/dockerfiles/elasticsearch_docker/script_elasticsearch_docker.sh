752997513cdbfd5e8ca0eb3909e877ce31ec77ad
root <jordibasktbdn9@gmail.com>
2016-05-18 10:02:48 +0200

    docker containers

--- docker/dockerfiles/elasticsearch_docker/script_elasticsearch_docker.sh
+++ docker/dockerfiles/elasticsearch_docker/script_elasticsearch_docker.sh
@@ -2,13 +2,13 @@
 
 
 # a partir d'un docker file esta crean una imatge
-docker build -t "imatge_kibana" . 
+docker build -t "imatge_elasticsearch" . 
 
 # crear un container a partir d'una iamtge docker
-docker create  --name "kibana_cont" imatge_kibana
+docker create  --name "elasticsearch_cont" -p 9200:9200 imatge_elasticsearch 
 
 
 # enjegar el container
-docker start kibana_cont
+docker start elasticsearch_cont
 
 
