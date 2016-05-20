acbe2f52f91a3ad5ceb1713120ce1d37d18e595b
isx48224562 <isx48224562@i14.informatica.escoladeltreball.org>
2016-05-20 13:11:13 +0200

    docker projecte

---
+++ docker/dockerfiles/script_docker.sh
@@ -0,0 +1,46 @@
+####################################################
+#                                                  #
+#                                                  #
+####################################################
+
+# Apaga i borra els containers ja existens (si hi són!).
+
+docker stop logstash_cont 2> /dev/null
+docker rm logstash_cont 2> /dev/null
+
+docker stop elasticsearch_cont 2> /dev/null
+docker rm elasticsearch_cont 2> /dev/null
+
+docker stop kibana_cont 2> /dev/null
+docker rm kibana_cont 2> /dev/null
+
+
+
+
+# a partir d'un docker file esta crean una imatge
+docker build --tag "imatge_logstash" logstash_docker 
+
+docker build --tag "imatge_elasticsearch" elasticsearch_docker
+
+docker build --tag "imatge_kibana" kibana_docker
+
+
+# crear un container a partir d'una iamtge docker
+
+docker create \
+	--name "elasticsearch_cont" \
+	--volume "/var/log/:/var/log/" \
+	--publish 9200:9200 \
+	imatge_elasticsearch 
+
+docker create  \
+        --name "logstash_cont" \
+        --link elasticsearch_cont:Elastic \
+        imatge_logstash
+
+docker create \
+	 --name "kibana_cont"  \
+	--link elasticsearch_cont:Elastic \
+	--publish 5601:5601 \
+	imatge_kibana
+
--- docker/dockerfiles/start_cont.sh
+++ docker/dockerfiles/start_cont.sh
@@ -1,5 +1,10 @@
 # script que encen tots els dockers
 
-docker start logstash_cont
+
+ # ojo que el logstash esta linkat amb elastic i petara si s'enjega abanns
+
 docker start elasticsearch_cont
+
+docker start logstash_cont
+
 docker start kibana_cont
